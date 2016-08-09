9ae595b80692187239067c1d32437510cf49f71c
IlianaP <ip97@cornell.edu>
2016-08-09 20:22:59 +0000

    starting a test for an edit action

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,5 +6,10 @@ FactoryGirl.define do
     password "secretPassword"
     password_confirmation "secretPassword"
     
+  end 
+
+  factory :gram do 
+  	message "hello"
+  	association :user 
   end
 end
